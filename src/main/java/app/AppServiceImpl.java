ef92fa1d1df50b65594f185190cb917066345c23
ParkJW <pc224538@gmail.com>
2022-12-29 18:02:41 +0900

    Update AppServiceImpl.java

--- src/main/java/app/AppServiceImpl.java
+++ src/main/java/app/AppServiceImpl.java
@@ -1,8 +1,11 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppServiceImpl implements AppService {
+	
+	@Autowired private AppDAO dao;
 
 }
