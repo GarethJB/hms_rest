7290cbed765a7bb818f152cd8fbf0e9614751df8
ParkJW <pc224538@gmail.com>
2022-12-29 18:01:00 +0900

    app package
    앱 패키지 생성

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -1,10 +1,13 @@
-package com.groupc.hms;
+package app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+	
+	@Autowired private AppServiceImpl service;
 	
 	@RequestMapping(value = "/test.ap")
 	public String appTest() {
